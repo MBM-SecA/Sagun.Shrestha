d025d8974ca09654d553b09544a92a5b83122fa1
Sagunstha <sagunashrestha76@gmail.com>
2020-09-24 08:38:07 +0545

    Assignment2 completed

---
+++ Assignnment2/FileDirectories.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Linq;
+public class FileIO
+{
+    public void LearnDirectories()
+    {
+        Directory.CreateDirectory(@"../ABC");
+        string fileHello = File.ReadAllText("Hello.cs");
+        for (int i = 0; i < 10; i++)
+        {
+            Directory.CreateDirectory($"../ABC/Folder{i + 1}");
+            File.WriteAllText($"../ABC/Folder{i + 1}/Hello.cs", fileHello);
+        }
+    }
+    static void Main (string[] args)
+    {
+        FileIO fileIO = new FileIO();
+        fileIO.LearnDirectories();
+    }
+} 
