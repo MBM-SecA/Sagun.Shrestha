0c9b37c28b71699a2aaa0cff461bee12b11ed625
Sagunstha <sagunashrestha76@gmail.com>
2020-10-11 15:42:20 +0545

    Added Detail

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -5,15 +5,20 @@ public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-
-        Person empl1 = new Person() {FirstName= "Bishnu", SurName = "Rawal",Address = "Kathmandu",Salary = 2000.0};
-        Person empl2 = new Person() {FirstName= "Bishnu", SurName = "Rawal",Address = "Kathmandu",Salary = 2000.0};
-        Person empl3 = new Person() {FirstName= "Bishnu", SurName = "Rawal",Address = "Kathmandu",Salary = 2000.0};
-        Person empl4 = new Person() {FirstName= "Bishnu", SurName = "Rawal",Address = "Kathmandu",Salary = 2000.0};
-        Person empl5 = new Person() {FirstName= "Bishnu", SurName = "Rawal",Address = "Kathmandu",Salary = 2000.0};
-        Person empl6 = new Person() {FirstName= "Bishnu", SurName = "Rawal",Address = "Kathmandu",Salary = 2000.0};
+        //Object initializer syntax
+        Person empl1 = new Person() {FirstName= "Sehun", SurName = "Oh",Address = "Kathmandu",Salary = 2000.0};
+        Person empl2 = new Person() {FirstName= "Kai", SurName = "Kim",Address = "Kathmandu",Salary = 2000.0};
+        Person empl3 = new Person() {FirstName= "Chanyeol", SurName = "Park",Address = "Kathmandu",Salary = 2000.0};
+        Person empl4 = new Person() {FirstName= "Chen", SurName = "Kim",Address = "Kathmandu",Salary = 2000.0};
+        Person empl5 = new Person() {FirstName= "Baekhyun", SurName = "Byun",Address = "Kathmandu",Salary = 2000.0};
+        Person empl6 = new Person() {FirstName= "Lay", SurName = "Zhang",Address = "Kathmandu",Salary = 2000.0};
         List<Person> employees = new List<Person>() {empl1, empl2, empl3, empl4, empl5, empl6};
         return View(employees);  
+    }
+
+    public ActionResult Detail(string firstName)
+    {
+        return View();
     }
 }
 
