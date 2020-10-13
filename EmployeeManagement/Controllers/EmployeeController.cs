9be9c45d8e5182ac1013344a5db866b40c581cd3
Sagunstha <sagunashrestha76@gmail.com>
2020-10-13 12:46:03 +0545

    Added Changes

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,24 +1,22 @@
-using System.Collections.Generic;
+using System;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-        //Object initializer syntax
-        Person empl1 = new Person() {FirstName= "Sehun", SurName = "Oh",Address = "Kathmandu",Salary = 2000.0};
-        Person empl2 = new Person() {FirstName= "Kai", SurName = "Kim",Address = "Kathmandu",Salary = 2000.0};
-        Person empl3 = new Person() {FirstName= "Chanyeol", SurName = "Park",Address = "Kathmandu",Salary = 2000.0};
-        Person empl4 = new Person() {FirstName= "Chen", SurName = "Kim",Address = "Kathmandu",Salary = 2000.0};
-        Person empl5 = new Person() {FirstName= "Baekhyun", SurName = "Byun",Address = "Kathmandu",Salary = 2000.0};
-        Person empl6 = new Person() {FirstName= "Lay", SurName = "Zhang",Address = "Kathmandu",Salary = 2000.0};
-        List<Person> employees = new List<Person>() {empl1, empl2, empl3, empl4, empl5, empl6};
-        return View(employees);  
+        var employees = Person.GetEmployees();
+        
+        return View(employees);
     }
-
-    public ActionResult Detail(string firstName)
+    
+    public ActionResult Detail([FromQuery]Guid id)
     {
-        return View();
+        var employees = Person.GetEmployees();
+        var employee = employees.FirstOrDefault(x => x.Id.ToString() == id.ToString);
+        
+        return View(employee);
     }
 }
 
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -1,8 +1,24 @@
+using System.Collections.Generic;
+using System;
 public class Person
 {
+    public Guid Id { get; set; }
     public string FirstName { get; set; }
     public string SurName { get; set; }
     public string Address { get; set; }
     public char Gender { get; set; } = 'M';
     public double Salary { get; set; }
+
+    public static List<Person> GetEmployees()
+    {
+        Person empl1 = new Person() { Id=Guid.NewGuid(), FirstName= "Sehun", SurName = "Oh",Address = "Kathmandu",Salary = 2000.0};
+        Person empl2 = new Person() { Id=Guid.NewGuid(), FirstName= "Kai", SurName = "Kim",Address = "Kathmandu",Salary = 2000.0};
+        Person empl3 = new Person() { Id=Guid.NewGuid(), FirstName= "Chanyeol", SurName = "Park",Address = "Kathmandu",Salary = 2000.0};
+        Person empl4 = new Person() { Id=Guid.NewGuid(), FirstName= "Chen", SurName = "Kim",Address = "Kathmandu",Salary = 2000.0};
+        Person empl5 = new Person() { Id=Guid.NewGuid(), FirstName= "Baekhyun", SurName = "Byun",Address = "Kathmandu",Salary = 2000.0};
+        Person empl6 = new Person() { Id=Guid.NewGuid(), FirstName= "Lay", SurName = "Zhang",Address = "Kathmandu",Salary = 2000.0};
+        List<Person> employees = new List<Person>() { empl1, empl2, empl3, empl4, empl5, empl6 };
+        return employees;
+        
+    }
 }
